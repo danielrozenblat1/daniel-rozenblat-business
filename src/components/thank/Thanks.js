5d057703591e2940b4fb0ade023adfc8150e8026
Your Name <you@example.com>
2024-06-19 13:08:36 +0300

    withoutvideo

--- src/components/thank/Thanks.js
+++ src/components/thank/Thanks.js
@@ -7,7 +7,7 @@ const Thanks=()=>{
 return <>
 <div className={styles.title}>תודה על האמון בי ! </div>
 <div className={styles.subtitle}>אתה בהחלט בדרך הנכונה ! <br/> עד השיחה שלנו.. מוזמן לעיין שוב בדפים שהכנתי ללקוחות שלי</div>
-<div className={styles.center}><video className={styles.video} src={daniel} alt="דניאל רוזנבלט סרטון תודה" autoPlay loop muted playsInline controls/></div>
+{/* <div className={styles.center}><video className={styles.video} src={daniel} alt="דניאל רוזנבלט סרטון תודה" autoPlay loop muted playsInline controls/></div> */}
 <Works/>
 </>
 
