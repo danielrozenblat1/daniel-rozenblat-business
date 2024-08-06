1d52b42b51a2457f0489c948fdfa7b5491056313
Your Name <you@example.com>
2024-08-06 17:35:41 +0300

    addedSentence

--- src/components/thank/Thanks.js
+++ src/components/thank/Thanks.js
@@ -10,7 +10,9 @@ return <>
 <div className={styles.subtitle}>אתה בהחלט בדרך הנכונה ! <br/> עד השיחה שלנו.. מוזמן לעיין שוב בדפים שהכנתי ללקוחות שלי</div>
 {/* <div className={styles.center}><video className={styles.video} src={daniel} alt="דניאל רוזנבלט סרטון תודה" autoPlay loop muted playsInline controls/></div> */}
 <Works/>
+
 <Recommends/>
+<div className={styles.subtitle}>כולי תקווה שאת/ה תהיה התמונה הבאה כאן</div>
 </>
 
 
