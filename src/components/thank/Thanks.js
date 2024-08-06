612aee8acd79d0ac52f5794174e8da6a77880b75
Your Name <you@example.com>
2024-08-06 16:59:11 +0300

    addedReccomnedsToThanks

--- src/components/thank/Thanks.js
+++ src/components/thank/Thanks.js
@@ -1,6 +1,7 @@
 import Works from "../works/Works"
 import styles from "./Thanks.module.css"
 import daniel from "../../videos/דניאל רוזנבלט סרטון תודה.mp4"   
+import Recommends from "../recommends/Recommends"
 const Thanks=()=>{
 
 
@@ -9,6 +10,7 @@ return <>
 <div className={styles.subtitle}>אתה בהחלט בדרך הנכונה ! <br/> עד השיחה שלנו.. מוזמן לעיין שוב בדפים שהכנתי ללקוחות שלי</div>
 {/* <div className={styles.center}><video className={styles.video} src={daniel} alt="דניאל רוזנבלט סרטון תודה" autoPlay loop muted playsInline controls/></div> */}
 <Works/>
+<Recommends/>
 </>
 
 
