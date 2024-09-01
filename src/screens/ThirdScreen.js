681164edce8d4ff60031c77c25510f6edf4bd5eb
Your Name <you@example.com>
2024-09-01 11:47:42 +0300

    removedSom

--- src/screens/ThirdScreen.js
+++ src/screens/ThirdScreen.js
@@ -18,14 +18,14 @@ return <>
 {/* <div className={styles.title}>איך אני הופך ליד קר לרותח?</div>
 {/* <div className={styles.center}><img className={styles.image} src={daniel} alt="דניאל רוזנבלט דפי נחיתה"/></div>  */}
 {/* <h1 className={styles.title}>דף נחיתה</h1> */}
-<h2 className={styles.subTitle} itemscope itemtype="http://schema.org/WebPage">אז מה זה בעצם דף נחיתה?</h2>
+{/* <h2 className={styles.subTitle} itemscope itemtype="http://schema.org/WebPage">אז מה זה בעצם דף נחיתה?</h2>
 <div className={styles.description}>דף נחיתה הוא אתר אינטרנטי שכשמו כן הוא - דף -  
  שבו נוחתים כל הלידים מהשיווק שלך.
 כשליד מסתקרן ממך כשהוא נחשף אליך מהשיווק הוא מגיע עם כמות חסמים עצומה ו 0 אמון בך מהסיבה הפשוטה - רדאר הבולשיט היום גבוה ואנשים כבר לא מאמינים לכל מה שמוכרים להם
 </div>
 <div className={styles.subTitle} itemprop="headline">הם רוצים לראות הוכחות !</div>
-<h1 className={`${styles.title} ${styles.bold}`} itemprop="headline" >וכאן היתרון של דף הנחיתה</h1>
-<h2 className={styles.subTitle} itemprop="description">יש לו 3 תפקידים עיקריים והם..</h2>
+<h1 className={`${styles.title} ${styles.bold}`} itemprop="headline" >וכאן היתרון של דף הנחיתה</h1> */}
+<h2 className={styles.subTitle} itemprop="description">לדף נחיתה יש 3 תפקידים עיקריים והם</h2>
 <Opposites  icon1={hot}  icon2={noResist}  icon3={tadmit} />
 </div>
 </>
